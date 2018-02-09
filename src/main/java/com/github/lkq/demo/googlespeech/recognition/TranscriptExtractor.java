882cac77090e6bf978a960a18098afcdac1688fb
kliu <kingson708@gmail.com>
2018-02-09 23:39:24 +0800

    fix NPE when no recognition result returned

--- src/main/java/com/github/lkq/demo/googlespeech/recognition/TranscriptExtractor.java
+++ src/main/java/com/github/lkq/demo/googlespeech/recognition/TranscriptExtractor.java
@@ -13,7 +13,13 @@ public class TranscriptExtractor {
     public String extractFromJson(String content) {
         JsonObject response = (JsonObject) new JsonParser().parse(content);
         JsonArray results = response.getAsJsonArray("results");
+        if (results == null || results.size() <= 0) {
+            return "(no results)";
+        }
         JsonArray alternatives = results.get(0).getAsJsonObject().getAsJsonArray("alternatives");
+        if (alternatives == null || alternatives.size() <= 0) {
+            return "(no results)";
+        }
 
         return alternatives.get(0).getAsJsonObject().get("transcript").getAsString();
     }
